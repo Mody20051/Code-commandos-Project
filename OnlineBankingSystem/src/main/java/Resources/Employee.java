60759bd5e052e6a7dd389c2e99ff5f3db022d134
Mody <mohamedahmedattia27@gmail.com>
2024-04-26 22:30:32 +0300

    Add files via upload

--- OnlineBankingSystem/src/main/java/Resources/Employee.java
+++ OnlineBankingSystem/src/main/java/Resources/Employee.java
@@ -1,6 +1,51 @@
 package Resources;
 
 public class Employee extends User{
-    pu
+    private String name;
+    private int id;
+    private int salary;
+    private int hours;
 
+    Employee(){
+
+    }
+
+    public Employee(String name, int id, int salary, int hours) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+        this.hours = hours;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
 }
--- OnlineBankingSystem/src/main/java/Resources/Manger.java
+++ OnlineBankingSystem/src/main/java/Resources/Manger.java
@@ -1,11 +1,22 @@
 package Resources;
 
 public class Manger extends Employee  {
-    private   String department;
+    private String department;
 
     public Manger() {
     }
-
-
+    public Manger(String name, int id, int salary, int hours, String department) {
+        super(name, id, salary, hours);
+        this.department = department;
     }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+}
 
