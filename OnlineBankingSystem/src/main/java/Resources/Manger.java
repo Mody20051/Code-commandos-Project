d9d83a81d5d92e3c5e4551f8dd4b0462b850e839
Mody <mohamedahmedattia27@gmail.com>
2024-04-27 21:40:53 +0300

    Add files via upload LOL

--- OnlineBankingSystem/src/main/java/Resources/Manger.java
+++ OnlineBankingSystem/src/main/java/Resources/Manger.java
@@ -1,22 +1,39 @@
 package Resources;
 
 public class Manger extends Employee  {
-    private String department;
+    private String branchID;
+    private String email;
 
-    public Manger() {
-    }
-    public Manger(String name, int id, int salary, int hours, String department) {
-        super(name, id, salary, hours);
-        this.department = department;
-    }
 
-    public String getDepartment() {
-        return department;
+
+
+
+    public Manger(String name, int phonenum, String address, Gender gender, int id, User user, String EID, int salary, int hours) {
+        super(name, phonenum, address, gender, id, user, EID, salary, hours);
     }
 
-    public void setDepartment(String department) {
-        this.department = department;
+    public String getBranchID() {
+        return branchID;
     }
 
+    public void setBranchID(String branchID) {
+        this.branchID = branchID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Manger{" +
+                "branchID='" + branchID + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
 
