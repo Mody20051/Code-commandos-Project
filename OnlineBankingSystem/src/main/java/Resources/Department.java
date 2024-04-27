f8c5fab4a8d982a697ef986eeff64c72f8bc293b
Mody <mohamedahmedattia27@gmail.com>
2024-04-27 21:40:53 +0300

    Add files via upload LOL

---
+++ OnlineBankingSystem/src/main/java/Resources/Department.java
@@ -0,0 +1,44 @@
+
+package Resources;
+import java .util.Date;
+public class Department {
+
+    private String DENAME;
+    private int DENUMBER;
+
+private  Date  MGSTATDATE;
+
+
+    public Department() {
+    }
+
+    public Department(String DENAME, int DENUMBER, Date MGSTATDATE) {
+        this.DENAME = DENAME;
+        this.DENUMBER = DENUMBER;
+        this.MGSTATDATE = MGSTATDATE;
+    }
+
+    public String getDENAME() {
+        return DENAME;
+    }
+
+    public void setDENAME(String DENAME) {
+        this.DENAME = DENAME;
+    }
+
+    public int getDENUMBER() {
+        return DENUMBER;
+    }
+
+    public void setDENUMBER(int DENUMBER) {
+        this.DENUMBER = DENUMBER;
+    }
+
+    public Date getMGSTATDATE() {
+        return MGSTATDATE;
+    }
+
+    public void setMGSTATDATE(Date MGSTATDATE) {
+        this.MGSTATDATE = MGSTATDATE;
+    }
+}
